54b739ee2634e35adfc5ba4ff93803d4ceb66313
Ricard0-A <ricardoarias4@hotmail.com>
2024-10-19 19:47:53 +0000

    done

---
+++ app.js
@@ -0,0 +1,12 @@
+  let pronoun = ['the', 'our', 'his'];
+  let adj = ['great', 'big', 'amazing' ];
+  let noun = ['jogger', 'racoon', 'dog', 'merchant', 'driver'];
+  
+
+  for(let i = 0; i < pronoun.length; i++) {
+    for(let j = 0; j < adj.length; j++) {
+      for(let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]+'.com'); 
+      }
+    }
+  }
